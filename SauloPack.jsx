e8d5f341dc706e6b43dbfb70b03c72e3c1db4b71
saulodalpozzo <saulodalpozzo@gmail.com>
2026-05-19 09:54:10 +0200

    Update SauloPack.jsx

--- SauloPack.jsx
+++ SauloPack.jsx
@@ -1,18 +1,73 @@
 /*
-    Text Animator UI Panel
-    Save as:
-    TextAnimator.jsx
-
-    Put in:
-    Adobe After Effects > Support Files > Scripts > ScriptUI Panels
-
-    Restart After Effects
-
-    Open:
-    Window > TextAnimator
+    SauloPack.jsx
+    Put in: Adobe After Effects > Support Files > Scripts > ScriptUI Panels
 */
 
-(function TextAnimatorPanel(thisObj){
+(function SauloPack(thisObj){
+
+    // =================================================
+    // HELPERS
+    // =================================================
+
+    function getActiveComp(){
+        var comp = app.project.activeItem;
+
+        if (!(comp instanceof CompItem)){
+            alert("Please select a composition.");
+            return null;
+        }
+
+        return comp;
+    }
+
+    function getSelectedProps(comp){
+        var props = comp.selectedProperties;
+
+        if (!props || props.length === 0){
+            alert("Please select at least one property.");
+            return null;
+        }
+
+        return props;
+    }
+
+    function applyExpressionToSelectedProps(expr, undoName){
+
+        app.beginUndoGroup(undoName);
+
+        try{
+            var comp = getActiveComp();
+            if (!comp) return;
+
+            var props = getSelectedProps(comp);
+            if (!props) return;
+
+            for (var i = 0; i < props.length; i++){
+                try{
+                    if (props[i].canSetExpression){
+                        props[i].expression = expr;
+                    }
+                }catch(err){}
+            }
+
+        }finally{
+            app.endUndoGroup();
+        }
+    }
+
+    function addSlider(layer, name, value){
+        var effects = layer.property("ADBE Effect Parade");
+        var slider = effects.addProperty("ADBE Slider Control");
+
+        slider.name = name;
+        slider.property("ADBE Slider Control-0001").setValue(value);
+
+        return slider;
+    }
+
+    // =================================================
+    // UI
+    // =================================================
 
     function buildUI(thisObj){
 
@@ -22,231 +77,94 @@
 
         win.orientation = "column";
         win.alignChildren = ["fill","top"];
+        win.spacing = 6;
+        win.margins = 10;
+
+        var btnTextAnimator = win.add("button", undefined, "Create Text Animator");
+        var btnBounce = win.add("button", undefined, "Bounce");
+        var btnVelocityBounce = win.add("button", undefined, "Velocity Bounce");
+        var btnMultiSlider = win.add("button", undefined, "Multi Slider Link");
+        var btnCenterTrim = win.add("button", undefined, "Center Trim");
 
         // =================================================
-        // BUTTONS
-        // =================================================
-
-        // -----------------------------------
         // CREATE TEXT ANIMATOR
-        // -----------------------------------
-
-        var btnTextAnimator =
-            win.add("button", undefined, "TextAnimator");
-
-        // -----------------------------------
-        // BASIC BOUNCE
-        // -----------------------------------
-
-        var btnBounce =
-            win.add("button", undefined, "Bounce");
-
-        // -----------------------------------
-        // SLIDER CONTROLS
-        // -----------------------------------
-
-        var btnMultiSlider =
-            win.add("button", undefined, "Multi Slider Link");
-
-        // -----------------------------------
-        // VELOCITY BOUNCE
-        // -----------------------------------
-
-        var btnVelocityBounce =
-            win.add("button", undefined, "Velocity Bounce");
-
-        // -----------------------------------
-        // CENTER TRIM
-        // -----------------------------------
-
-        var btnCenterTrim =
-            win.add("button", undefined, "Center Trim");
-
-        // =================================================
-        // TEXT ANIMATOR BUTTON
         // =================================================
 
         btnTextAnimator.onClick = function(){
 
             app.beginUndoGroup("Create Text Animator");
 
-            var comp = app.project.activeItem;
-
-            if (comp && comp instanceof CompItem){
-
-                // -----------------------------------
-                // CREATE TEXT LAYER
-                // -----------------------------------
+            try{
+                var comp = getActiveComp();
+                if (!comp) return;
 
                 var textLayer = comp.layers.addText("Text");
                 textLayer.name = "Text";
 
-                // -----------------------------------
-                // EFFECTS
-                // -----------------------------------
+                addSlider(textLayer, "Anim", 100);
+                addSlider(textLayer, "Delay", 0.05);
 
-                var effects =
-                    textLayer.property("ADBE Effect Parade");
-
-                // -----------------------------------
-                // ANIM SLIDER
-                // -----------------------------------
-
-                var animSlider =
-                    effects.addProperty("ADBE Slider Control");
-
-                animSlider.name = "Anim";
-
-                var animProp =
-                    animSlider.property("ADBE Slider Control-0001");
+                var animProp = textLayer
+                    .property("ADBE Effect Parade")
+                    .property("Anim")
+                    .property("ADBE Slider Control-0001");
 
                 animProp.setValueAtTime(0, 100);
                 animProp.setValueAtTime(1, 0);
 
-                // -----------------------------------
-                // DELAY SLIDER
-                // -----------------------------------
+                var textProps = textLayer.property("ADBE Text Properties");
+                var animators = textProps.property("ADBE Text Animators");
 
-                var delaySlider =
-                    effects.addProperty("ADBE Slider Control");
-
-                delaySlider.name = "Delay";
-
-                delaySlider
-                    .property("ADBE Slider Control-0001")
-                    .setValue(0.05);
-
-                // -----------------------------------
-                // TEXT ANIMATOR
-                // -----------------------------------
-
-                var textProps =
-                    textLayer.property("ADBE Text Properties");
-
-                var animators =
-                    textProps.property("ADBE Text Animators");
-
-                var animator =
-                    animators.addProperty("ADBE Text Animator");
-
+                var animator = animators.addProperty("ADBE Text Animator");
                 animator.name = "Animator01";
 
-                // -----------------------------------
-                // POSITION PROPERTY
-                // -----------------------------------
+                var animatorProps = animator.property("ADBE Text Animator Properties");
 
-                var animatorProps =
-                    animator.property("ADBE Text Animator Properties");
+                var posProp = animatorProps.addProperty("ADBE Text Position 3D");
+                posProp.setValue([0, 25, 0]);
 
-                var posProp =
-                    animatorProps.addProperty("ADBE Text Position 3D");
+                var selectors = animator.property("ADBE Text Selectors");
 
-                posProp.setValue([0,0,0]);
+                var exprSelector = selectors.addProperty("ADBE Text Expressible Selector");
+                exprSelector.name = "Per Character Delay";
 
-                // -----------------------------------
-                // EXPRESSION SELECTOR
-                // -----------------------------------
-
-                var selectors =
-                    animator.property("ADBE Text Selectors");
-
-                var exprSelector =
-                    selectors.addProperty("ADBE Text Expressible Selector");
-
-                var amount =
-                    exprSelector.property("ADBE Text Expressible Amount");
+                var amount = exprSelector.property("ADBE Text Expressible Amount");
 
                 amount.expression =
                     'anim = effect("Anim")("Slider");\r' +
                     'delay = effect("Delay")("Slider");\r' +
-                    'id = textIndex;\r' +
-                    'd = delay * id;\r\r' +
-                    'anim.valueAtTime(time-d);';
+                    'd = delay * (textIndex - 1);\r\r' +
+                    'anim.valueAtTime(time - d);';
 
-            }else{
-
-                alert("Please select a composition.");
-
+            }finally{
+                app.endUndoGroup();
             }
-
-            app.endUndoGroup();
         };
 
         // =================================================
-        // BASIC BOUNCE BUTTON
+        // BASIC BOUNCE
         // =================================================
 
         btnBounce.onClick = function(){
 
-            app.beginUndoGroup("Apply Bounce Expression");
-
-            var comp = app.project.activeItem;
-
-            if (!(comp instanceof CompItem)){
-                alert("Please select a composition.");
-                return;
-            }
-
-            var selectedProps = comp.selectedProperties;
-
-            if (selectedProps.length === 0){
-                alert("Please select a property.");
-                return;
-            }
-
             var expr =
-
                 'freq = 3;\r' +
-                'decay = 10;\r\r' +
+                'decay = 8;\r' +
+                'amp = 20;\r\r' +
 
                 't = time - inPoint;\r' +
-                'startVal = [50,50];\r' +
-                'endVal = [100,100];\r' +
-                'dur = 0.05;\r\r' +
+                'value + amp * Math.sin(freq * t * 2 * Math.PI) / Math.exp(decay * t);';
 
-                'if (t < dur){\r' +
-                '    linear(t,0,dur,startVal,endVal);\r' +
-                '}else{\r' +
-                '    amp = (endVal - startVal)/dur;\r' +
-                '    w = freq*Math.PI*2;\r' +
-                '    endVal + amp*(Math.sin((t-dur)*w)/Math.exp(decay*(t-dur))/w);\r' +
-                '}';
-
-            for (var i = 0; i < selectedProps.length; i++){
-
-                try{
-                    selectedProps[i].expression = expr;
-                }catch(err){}
-
-            }
-
-            app.endUndoGroup();
+            applyExpressionToSelectedProps(expr, "Apply Bounce Expression");
         };
 
         // =================================================
-        // VELOCITY BOUNCE BUTTON
+        // VELOCITY BOUNCE
         // =================================================
 
         btnVelocityBounce.onClick = function(){
 
-            app.beginUndoGroup("Apply Velocity Bounce");
-
-            var comp = app.project.activeItem;
-
-            if (!(comp instanceof CompItem)){
-                alert("Please select a composition.");
-                return;
-            }
-
-            var selectedProps = comp.selectedProperties;
-
-            if (selectedProps.length === 0){
-                alert("Please select a property.");
-                return;
-            }
-
             var expr =
-
                 'freq = 4;\r' +
                 'decay = 6;\r' +
                 'amp = 0.05;\r\r' +
@@ -261,163 +179,109 @@
                 '}\r\r' +
 
                 'if (n > 0){\r' +
-
                 '    t = time - key(n).time;\r' +
-
-                '    v = velocityAtTime(\r' +
-                '        key(n).time - thisComp.frameDuration/10\r' +
-                '    ) * amp;\r\r' +
-
-                '    value +\r' +
-                '    v *\r' +
-                '    (Math.sin(freq*t*2*Math.PI)/Math.exp(decay*t))/freq;\r' +
-
+                '    v = velocityAtTime(key(n).time - thisComp.frameDuration/10) * amp;\r' +
+                '    value + v * (Math.sin(freq * t * 2 * Math.PI) / Math.exp(decay * t)) / freq;\r' +
                 '}else{\r' +
                 '    value;\r' +
                 '}';
 
-            for (var i = 0; i < selectedProps.length; i++){
-
-                try{
-                    selectedProps[i].expression = expr;
-                }catch(err){}
-
-            }
-
-            app.endUndoGroup();
+            applyExpressionToSelectedProps(expr, "Apply Velocity Bounce");
         };
 
         // =================================================
-        // SLIDER CONTROLS BUTTON
+        // MULTI SLIDER LINK
         // =================================================
 
         btnMultiSlider.onClick = function(){
 
-    app.beginUndoGroup("Multi Slider Link");
+            app.beginUndoGroup("Multi Slider Link");
 
-    var comp = app.project.activeItem;
+            try{
+                var comp = getActiveComp();
+                if (!comp) return;
 
-    if (!(comp instanceof CompItem)){
-        alert("Select a composition.");
-        return;
-    }
+                var layers = comp.selectedLayers;
+                var props = getSelectedProps(comp);
 
-    var layers = comp.selectedLayers;
-    var props = comp.selectedProperties;
+                if (!props) return;
 
-    if (layers.length === 0 || props.length === 0){
-        alert("Select a layer + a property.");
-        return;
-    }
+                if (!layers || layers.length === 0){
+                    alert("Select a layer to hold the sliders.");
+                    return;
+                }
 
-    var layer = layers[0];
-    var effects = layer.property("ADBE Effect Parade");
+                var layer = layers[0];
 
-    // --------------------------------------------
-    // ASK USER FOR NUMBER OF CONTROLLERS
-    // --------------------------------------------
+                var n = parseInt(prompt("How many sliders?", "3"), 10);
 
-    var n = parseInt(prompt("How many sliders?", "3"), 10);
+                if (isNaN(n) || n < 1){
+                    alert("Invalid number.");
+                    return;
+                }
 
-    if (isNaN(n) || n < 1){
-        alert("Invalid number.");
-        return;
-    }
+                var expr = "value";
 
-    // --------------------------------------------
-    // CREATE SLIDERS + BUILD EXPRESSION
-    // --------------------------------------------
+                for (var i = 1; i <= n; i++){
 
-    var expr = "value";
+                    var name = "Slider Control " + i;
 
-    for (var i = 1; i <= n; i++){
+                    addSlider(layer, name, 0);
 
-        var name = "Slider Control " + i;
+                    expr += '\r+ effect("' + name + '")("Slider")';
+                }
 
-        var slider = effects.addProperty("ADBE Slider Control");
-        slider.name = name;
+                for (var p = 0; p < props.length; p++){
+                    try{
+                        if (props[p].canSetExpression){
+                            props[p].expression = expr;
+                        }
+                    }catch(err){}
+                }
 
-        expr += '\r+ effect("' + name + '")("Slider")';
-    }
-
-    // --------------------------------------------
-    // APPLY EXPRESSION TO PROPERTY
-    // --------------------------------------------
-
-    for (var p = 0; p < props.length; p++){
-
-        try{
-            if (props[p] && props[p].canSetExpression){
-                props[p].expression = expr;
+            }finally{
+                app.endUndoGroup();
             }
-        }catch(err){}
-    }
-
-    app.endUndoGroup();
-};
+        };
 
         // =================================================
-        // CENTER TRIM BUTTON
+        // CENTER TRIM
         // =================================================
 
         btnCenterTrim.onClick = function(){
 
             app.beginUndoGroup("Center Trim");
 
-            var comp = app.project.activeItem;
+            try{
+                var comp = getActiveComp();
+                if (!comp) return;
 
-            if (!(comp instanceof CompItem)){
-                alert("Select a composition.");
-                return;
-            }
+                var layers = comp.selectedLayers;
 
-            var layers = comp.selectedLayers;
-
-            if (layers.length === 0){
-                alert("Select a shape layer.");
-                return;
-            }
-
-            for (var i = 0; i < layers.length; i++){
-
-                var layer = layers[i];
-
-                // -----------------------------------
-                // SHAPE CONTENTS
-                // -----------------------------------
-
-                var contents =
-                    layer.property("ADBE Root Vectors Group");
-
-                if (!contents){
-                    continue;
+                if (!layers || layers.length === 0){
+                    alert("Select at least one shape layer.");
+                    return;
                 }
 
-                // -----------------------------------
-                // ADD TRIM PATHS
-                // -----------------------------------
+                for (var i = 0; i < layers.length; i++){
 
-                var trim =
-                    contents.addProperty("ADBE Vector Filter - Trim");
+                    var layer = layers[i];
+                    var contents = layer.property("ADBE Root Vectors Group");
 
-                // -----------------------------------
-                // GET START PROPERTY
-                // -----------------------------------
+                    if (!contents) continue;
 
-                var startProp =
-                    trim.property("ADBE Vector Trim Start");
+                    var trim = contents.addProperty("ADBE Vector Filter - Trim");
+                    trim.name = "Center Trim";
 
-                // -----------------------------------
-                // APPLY EXPRESSION
-                // -----------------------------------
+                    var startProp = trim.property("ADBE Vector Trim Start");
 
-                startProp.expression =
-                    '100 - content("'
-                    + trim.name +
-                    '").end';
+                    startProp.expression =
+                        '100 - content("' + trim.name + '").end';
+                }
+
+            }finally{
+                app.endUndoGroup();
             }
-
-            app.endUndoGroup();
         };
 
         // =================================================
@@ -425,6 +289,7 @@
         // =================================================
 
         win.layout.layout(true);
+        win.layout.resize();
 
         return win;
     }
